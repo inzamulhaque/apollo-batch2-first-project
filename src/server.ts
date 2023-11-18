2905da25e7c311bc8fe150a9b810a5265d1625be
inzamulhaque <mdihalif@gmail.com>
2023-11-18 23:43:09 +0600

    create student interface and schema

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-import app from "./app";
-import mongoose from "mongoose";
-import config from "./app/config";
+import app from './app';
+import mongoose from 'mongoose';
+import config from './app/config';
 const port = process.env.PORT || 7000;
 
 async function main() {
@@ -8,7 +8,7 @@ async function main() {
     await mongoose.connect(
       `mongodb+srv://${config.dbUser as string}:${
         config.dbPassword as string
-      }@cluster0.ehl62mb.mongodb.net/first-project?retryWrites=true&w=majority`
+      }@cluster0.ehl62mb.mongodb.net/first-project?retryWrites=true&w=majority`,
     );
 
     app.listen(port, () => {
